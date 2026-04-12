5ce15b6af12c185525d4d846102f69a58a12fa6f
ethiel-us <ethblacub@alum.us.es>
2026-04-12 22:02:50 +0200

    add dailymotionminer base project structure

---
+++ dailymotionminer/src/main/java/aiss/dailymotionminer/DailymotionminerApplication.java
@@ -0,0 +1,13 @@
+package aiss.dailymotionminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailymotionminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailymotionminerApplication.class, args);
+	}
+
+}
