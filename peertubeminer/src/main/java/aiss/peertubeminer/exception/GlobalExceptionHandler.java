813d80c9b06ec9586f02d5a02b1b758b784056ed
ethiel-us <ethblacub@alum.us.es>
2026-05-09 14:57:26 +0200

    add exception classes

---
+++ peertubeminer/src/main/java/aiss/peertubeminer/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package aiss.peertubeminer.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ChannelNotFoundException.class)
+    @ResponseBody
+    public ResponseEntity<Map<String, String>> handleChannelNotFoundException(ChannelNotFoundException ex) {
+        Map<String, String> res = new LinkedHashMap<>();
+        res.put("status", HttpStatus.NOT_FOUND.toString());
+        res.put("error", ex.getMessage());
+        return new ResponseEntity<>(res, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(VideoMinerCommunicationException.class)
+    @ResponseBody
+    public ResponseEntity<Map<String, String>> handleVideoMinerCommunicationException(VideoMinerCommunicationException ex) {
+        Map<String, String> res = new LinkedHashMap<>();
+        res.put("status", String.valueOf(ex.getStatus().value()));
+        res.put("error", ex.getStatus().getReasonPhrase());
+        res.put("message", ex.getMessage());
+        return new ResponseEntity<>(res, ex.getStatus());
+    }
+}
